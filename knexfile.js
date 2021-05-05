4930dc69ab9dc79d9e694238a5cd0003d9425971
Cesar Vaz <cesar_augusto_perfil@hotmail.com>
2021-05-05 11:25:38 -0300

    config de db_heroku

--- knexfile.js
+++ knexfile.js
@@ -2,12 +2,13 @@
 
 module.exports = {
 
-  development: {
+  production: {
     client: 'pg',
     connection: {
-      database: "knex_catedra",
-      user: "postgres",
-      password: "admin"
+      host: "ec2-34-200-94-86.compute-1.amazonaws.com",
+      database: "daveol1c3sn28q",  //nex_catedra
+      user: "qmfpgtmxrlwful",  //postgres
+      password: "8ee1132f35ccca83e0fac5a23b5692b20f7abfb70b1798a9cd247d5624912bfe"  //admin
     },
     migrations: {
       tableName: 'knex_migrations',
@@ -18,36 +19,36 @@ module.exports = {
     }
   },
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
+  // staging: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // },
 
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
+  // production: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // }
 
 };
