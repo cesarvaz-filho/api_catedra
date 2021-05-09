4e0caa7cd0c5fe48ddad40c5b020c16a031b2870
Cesar Vaz <cesar_augusto_perfil@hotmail.com>
2021-05-09 18:58:11 -0300

    segunda config de db_heroku

--- knexfile.js
+++ knexfile.js
@@ -2,13 +2,12 @@
 
 module.exports = {
 
-  production: {
+  development: {
     client: 'pg',
     connection: {
-      host: "ec2-34-200-94-86.compute-1.amazonaws.com",
-      database: "daveol1c3sn28q",  //nex_catedra
-      user: "qmfpgtmxrlwful",  //postgres
-      password: "8ee1132f35ccca83e0fac5a23b5692b20f7abfb70b1798a9cd247d5624912bfe"  //admin
+      database: "knex_catedra", 
+      user: "postgres", 
+      password: "admin" 
     },
     migrations: {
       tableName: 'knex_migrations',
@@ -35,20 +34,20 @@ module.exports = {
   //   }
   // },
 
-  // production: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // }
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: `${__dirname}/src/database/migrations`
+    },
+    seeds: {
+      directory: `${__dirname}/src/database/seeds`
+    }
+  }
 
 };
