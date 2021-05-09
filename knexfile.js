58e8cb50d199d35dc8ccb397ab07f49a5d637e28
Cesar Vaz <cesar_augusto_perfil@hotmail.com>
2021-05-09 20:58:05 -0300

    config knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -17,26 +17,13 @@ module.exports = {
       directory: `${__dirname}/src/database/seeds`
     }
   },
-
-  // staging: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // },
-
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
     migrations: {
       tableName: 'knex_migrations',
       directory: `${__dirname}/src/database/migrations`
