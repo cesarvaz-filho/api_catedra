0eb15f00ee7ea94d45d478246ab2d6a734780dc1
Cesar Vaz <cesar_augusto_perfil@hotmail.com>
2021-05-09 19:39:49 -0300

    arquivo knexfile.js corrigido

--- knexfile.js
+++ knexfile.js
@@ -37,10 +37,6 @@ module.exports = {
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL,
-    pool: {
-      min: 2,
-      max: 10
-    },
     migrations: {
       tableName: 'knex_migrations',
       directory: `${__dirname}/src/database/migrations`
