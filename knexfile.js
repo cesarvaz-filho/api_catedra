64fb9a727851049a72b2c6a930bbbe6935bf3863
Cesar Vaz <cesar_augusto_perfil@hotmail.com>
2021-05-09 20:13:51 -0300

    config do knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -36,7 +36,13 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL,
+    connection: {
+      host: "ec2-34-200-94-86.compute-1.amazonaws.com",
+      database: "daveol1c3sn28q", 
+      port: 5432,
+      user: "qmfpgtmxrlwful", 
+      password: "8ee1132f35ccca83e0fac5a23b5692b20f7abfb70b1798a9cd247d5624912bfe" 
+    },
     migrations: {
       tableName: 'knex_migrations',
       directory: `${__dirname}/src/database/migrations`
